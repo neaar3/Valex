7faf9801450f0ebb7e0516bf5c469df7e6417269
rodnei0 <rodnei0@gmail.com>
2022-04-18 17:01:46 -0400

    bug fixes

--- src/middlewares/ensureAuthMiddleware.ts
+++ src/middlewares/ensureAuthMiddleware.ts
@@ -1,21 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import * as authService from '../services/authService.js'
-import { unauthorizedError } from "./handleErrorsMiddleware.js";
+import { notFoundError, unauthorizedError } from "./handleErrorsMiddleware.js";
 
 export default async function ensureAuthMiddleware(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.headers['x-api-key'] as string;
 
-    if (!apiKey) {
-        return res.sendStatus(401)
-    }
+    if (!apiKey) throw notFoundError('API Key');
 
-    try {
-        const keyExist = await authService.verifyApiKey(apiKey);
-
-        if (keyExist === undefined) throw unauthorizedError('API Key')
-    } catch (error) {
-        console.log(error)
-    }
+    const keyExist = await authService.verifyApiKey(apiKey);
+    
+    if (keyExist === undefined) throw unauthorizedError('API Key');
 
     next();
 }
